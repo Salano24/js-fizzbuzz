1d05bec5c55bfd72d5055cd73071d79e3752395b
Salano24 <salernomarco48@email.com>
2022-10-10 15:26:42 +0200

    fine esercizio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,30 @@
-const numberEl = document.querySelector('.number');
+let numeri = document.querySelector("#numeri")
 
-for (let i = 1; i < 101; i++) {
-    
-    console.log(i);
+        let contenutoBlocco;
+        let stile;
 
-    numberEl.innerHTML += `<span> ${i} </span>`
-}
+        for(let i=1; i<=100; i++){
+
+            if(i%3==0){
+                contenutoBlocco += "Fitz"
+                stile ="orange"
+            }
+
+            if(i%5==0){
+                contenutoBlocco += "Buzz"
+                stile ="green"
+            }
+
+            if(i%5==0&&i%3==0){
+                stile="red"
+            }
+
+            if(i%3!=0 && i%5!=0){
+                contenutoBlocco = i
+                stile ="blue"
+            }
+
+            numeri.innerHTML += `<p class="${stile}">${contenutoBlocco}</p>`
+            
+            contenutoBlocco = ""
+        }
